ac20492f7a1fd2534de26a843e3e720f763b4d7c
CHEN, Jiaxing <kaseichincjx@outlook.com>
2020-10-13 20:52:32 +0800

    10132052

--- data_process/data_access.py
+++ data_process/data_access.py
@@ -14,9 +14,9 @@ yuanshi_shujv = pd.read_table('hld_and_rxd.fzp', encoding = 'gbk')
 
 # 提取有用数据
 shuzhi_shujv_col = yuanshi_shujv.iloc[13:]
-print(shuzhi_shujv_col)
+# print(shuzhi_shujv_col)
 
-'''
+
 shuzhi_shujv_fenge = pd.DataFrame([jj.split(';') for jj in shuzhi_shujv_col['Vehicle Record']])
 # print(shuzhi_shujv_fenge)
 
@@ -40,24 +40,6 @@ shuzhi_shujv_fenge['  Head'] = shuzhi_shujv_fenge['  Head'].astype(float)
 # 删除无效列
 shuzhi_shujv_fenge = shuzhi_shujv_fenge.drop([' '], axis =1)
 
-# print(shuzhi_shujv_fenge)
+print(shuzhi_shujv_fenge)
 
-data_sampled = []
 
-# ！！！需要修改的起始时间
-time = 2.0
-time_flag = 0
-
-for i in range(shuzhi_shujv_fenge.shape[0]):
-    if (shuzhi_shujv_fenge.iloc[i][5] != time ):
-        if (time_flag == 0):
-            time = time + 1
-            time_flag = 1
-    else:
-        time_flag = 0
-        data_sampled.append(list(shuzhi_shujv_fenge.iloc[i]))
-        
-
-print(data_sampled)
-
-'''
