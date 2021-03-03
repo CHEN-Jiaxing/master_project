a13aaee3ec716e7730e3bf6c39fa84a1454f3642
CHEN, Jiaxing <kaseichincjx@outlook.com>
2021-03-03 16:30:29 +0800

    debug

--- neural_network/mogai_demo.py
+++ neural_network/mogai_demo.py
@@ -4,27 +4,28 @@ from keras.models import Sequential
 from keras.layers import LSTM, TimeDistributed, Dense
 from keras.optimizers import Adam
 
+# 读取数据
 data = np.load('input.npy')
 
+# 网络参数设置
 BATCH_START = 0
-TIME_STEPS = 2
-BATCH_SIZE = 6
+TIME_STEPS = 5
+BATCH_SIZE = 10
 INPUT_SIZE = 5
 OUTPUT_SIZE = 2
 CELL_SIZE = 20
 LR = 0.006
 
 # 数据长度取整
-BATCH_NUM = int(len(data / BATCH_SIZE))
+BATCH_NUM = int(len(data) / BATCH_SIZE)
 data = data[0:BATCH_NUM * BATCH_SIZE]
 
+# 输入输出获取
 def batch():
     global BATCH_START, TIME_STEPS
     X_batch = data[BATCH_START: BATCH_START + BATCH_SIZE * TIME_STEPS, 1:6]
     X_batch = X_batch.reshape(BATCH_SIZE, TIME_STEPS,5)
     Y_batch = data[BATCH_START: BATCH_START + BATCH_SIZE * TIME_STEPS, 6:8]
-    print(Y_batch)
-    print(Y_batch.shape)
     Y_batch = Y_batch.reshape(BATCH_SIZE, TIME_STEPS,2)
     BATCH_START += TIME_STEPS
     return [X_batch, Y_batch]
@@ -45,12 +46,10 @@ model.compile(optimizer=adam,
 
 
 print('Training ------------')
-for step in range(BATCH_NUM):
+for step in range(int((len(data) - BATCH_SIZE * TIME_STEPS)/TIME_STEPS) + 1):
     # data shape = (batch_num, steps, inputs/outputs)
     X_batch, Y_batch = batch()
     cost = model.train_on_batch(X_batch, Y_batch)
     pred = model.predict(X_batch, BATCH_SIZE)
-    if step % 10 == 0:
-        print('train cost: ', cost)
-
-
+    # if step % 10 == 0:
+    print('train cost: ', cost)
